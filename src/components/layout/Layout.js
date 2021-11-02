3a98ef62e8cf9bbea40cbee6878f72f23cbb937f
quyenld9699 <quyenld9699@gmail.com>
2021-11-03 02:30:53 +0700

    set up project structure

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,24 @@
+import Footer from "./footer/Footer"
+import Header from "./header/Header"
+import Sidebar from "./sidebar/Sidebar"
+
+function Layout(props) {
+
+    return (
+        <div className="layout">
+            <div className="layout__header">
+                <Header titlePage={titlePage} breadcrumb={breadcrumb} />
+            </div>
+            <div className="layout__sidebar">
+                <input type="checkbox" id="toggle_sidebar" />
+                <Sidebar />
+                <label className="label_sidebar" htmlFor="toggle_sidebar"></label>
+            </div>
+            <div className="layout__content">{children}</div>
+            <div className="layout__footer">
+                <Footer />
+            </div>
+        </div>
+    )
+}
+export default Layout
